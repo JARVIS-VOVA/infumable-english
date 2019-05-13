fab3d76572f8defc6ff5bed24719e45837389d7b
JARVIS-VOVA <jarvis.vova@gmail.com>
2019-05-13 18:39:42 +0300

    Create migration users; Create user in seed

---
+++ db/migrations/20190608232154_create_users.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(t) {
+    t.increments('id').unsigned().primary()
+    t.dateTime('createdAt').notNull()
+    t.dateTime('updatedAt').nullable()
+    t.dateTime('deletedAt').nullable()
+
+    t.string('login').notNull()
+    t.string('password').notNull()
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users')
+}
