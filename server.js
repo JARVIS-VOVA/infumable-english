8852874db041789115653dcc53a2fb45f1b0876b
JARVIS-VOVA <jarvis.vova@gmail.com>
2019-05-28 19:09:09 +0300

    Create server.js

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
-// .ENV ---
-import dotenv from 'dotenv'
+import express from 'express'
+import bodyParser from 'body-parser'
 
-const configDotenv = dotenv.config(({ path: './config/.env' }))
+import _configDotenv from './config/dotenv'
+import { SUCCESS } from './constants/colorsForConsole'
 
-if (configDotenv.error) {
-  throw configDotenv.error
-}
+const app = express()
+const port = process.env.PORT || 3000
 
-console.log(configDotenv.parsed)
-// .ENV --- end
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// routes
+const users = require('./app/user/routes')
+
+app.use(users)
+
+// error handler
+app.use((req, res, next) => {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  })
+})
+
+app.listen(port, () => {
+  console.log(SUCCESS, `Listening on ${port}`)
+})
