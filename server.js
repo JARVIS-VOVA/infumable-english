881007e44ab5b3520706c7b9f923992ead13475f
JARVIS-VOVA <jarvis.vova@gmail.com>
2019-06-13 22:38:30 +0300

    Create server.js; Install and setting dotenv; Add .env to .gitignore; Create .env.example;

---
+++ server.js
@@ -0,0 +1,11 @@
+// .ENV ---
+import dotenv from 'dotenv'
+
+const configDotenv = dotenv.config(({ path: './config/.env' }))
+
+if (configDotenv.error) {
+  throw configDotenv.error
+}
+
+console.log(configDotenv.parsed)
+// .ENV --- end
