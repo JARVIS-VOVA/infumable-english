5489a31ec05981ff12da5f4c945650e64d3c7dd0
JARVIS-VOVA <jarvis.vova@gmail.com>
2019-07-02 00:54:19 +0300

    Move babelrc presets to webpack

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -27,7 +27,11 @@ module.exports = {
     {
       test: /\.js$/,
       loader: 'babel-loader',
-      exclude: '/node_modules/'
+      exclude: '/node_modules/',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ["@babel/plugin-proposal-export-default-from", '@babel/plugin-proposal-class-properties']
+      }
     }, {
       test: /\.(png|jpg|gif|svg)$/,
       loader: 'file -loader',
