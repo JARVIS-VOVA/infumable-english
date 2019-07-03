60cdb979fc535724ae3af473ac1458f2f15202ed
JARVIS-VOVA <jarvis.vova@gmail.com>
2019-07-03 17:06:08 +0300

    Init react files

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import '../assets/styles/app.scss'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Infumable english!</h1>
+      </div>
+    )
+  }
+}
+
+export default App
