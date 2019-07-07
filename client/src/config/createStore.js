fa4c48f046ecdcd7a3824a45202c1ba713c5fe58
JARVIS-VOVA <jarvis.vova@gmail.com>
2019-07-08 00:44:14 +0300

    Add saga-thunk

--- client/src/config/createStore.js
+++ client/src/config/createStore.js
@@ -1,13 +1,20 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { router5Middleware, router5Reducer } from 'redux-router5'
 import { createLogger } from 'redux-logger'
+import createSagaMiddleware from 'redux-saga'
+import { middleware as thunkMiddleware } from 'redux-saga-thunk'
 
 import reducers from '../store/reducers'
+import rootWatcherSaga from '../store/sagas'
 
 export default (router, initialState = {}) => {
+  const sagaMiddleware = createSagaMiddleware()
+
   const createStoreWithMiddleware = applyMiddleware(
     router5Middleware(router),
-    createLogger({ diff: true, collapsed: true })
+    createLogger({ diff: true, collapsed: true }),
+    thunkMiddleware,
+    sagaMiddleware
   )(createStore)
 
   const store = createStoreWithMiddleware(
@@ -17,6 +24,8 @@ export default (router, initialState = {}) => {
     }),
     initialState
   )
+
+  sagaMiddleware.run(rootWatcherSaga)
 
   window.store = store
 
