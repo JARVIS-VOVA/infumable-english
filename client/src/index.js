55e42d4bedc4297466c2c49d09ff59583b4468ad
JARVIS-VOVA <jarvis.vova@gmail.com>
2019-07-08 01:05:54 +0300

    Create App.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,24 +3,19 @@ import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router5'
 import ReactDOM from 'react-dom'
 
-import createRouter from './config/createRouter'
+import router from './config/createRouter'
 import configureStore from './config/createStore'
 
-import Nav from './components/pages/Nav'
-import SwitchRoute from './SwitchRoute'
+import App from './App'
 
 import './assets/styles/app.scss'
 
-const router = createRouter()
 const store = configureStore(router)
 
 const wrappedApp = (
   <Provider store={store}>
     <RouterProvider router={router}>
-      <>
-        <Nav />
-        <SwitchRoute />
-      </>
+      <App />
     </RouterProvider>
   </Provider>
 )
