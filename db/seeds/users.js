b4e0f3986780da16d832f3fe92147403f0fa2f38
JARVIS-VOVA <jarvis.vova@gmail.com>
2019-07-08 13:02:29 +0300

    Add email to model user

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -4,6 +4,7 @@ exports.seed = function(knex, Promise) {
       return knex('users').insert([
         {
           id: 1,
+          email: 'example@gmail.com',
           login: 'MaryPoppins',
           password: 'password',
           createdAt: new Date()
