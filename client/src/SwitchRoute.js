d8461b1cab44923dc75a1de6abcd01a53f5a089b
JARVIS-VOVA <jarvis.vova@gmail.com>
2019-07-12 16:56:22 +0300

    Create empty page 'Home'

--- client/src/SwitchRoute.js
+++ client/src/SwitchRoute.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux'
 import { routeNodeSelector } from 'redux-router5'
 
 import NotFound from './components/pages/NotFound'
-
+import Home from './components/pages/Home'
 import SignUp from './containers/SignUp'
 
 const SwitchRoute = ({ route }) => {
   const topRouteName = route.name.split('.')[0]
 
   if (topRouteName === 'signUp') return <SignUp />
+  if (topRouteName === 'home') return <Home />
 
   return <NotFound />
 }
---
+++ client/src/components/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Home = () => (
+  <h1 className='title'>WELCOME!</h1>
+)
+
+export default Home
