4a71d56278ecd788331019c4832bdf10aa92d931
Volodymyr Babii <jarvis@Volodymyrs-MacBook-Air.local>
2021-10-18 15:37:41 +0300

    Regenerate api (rails new)
    # Conflicts:
    #	api/Gemfile
    #	api/Gemfile.lock

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+# Remove a potentially pre-existing server.pid for Rails.
 rm -f tmp/pids/server.pid
 
 rails db:create db:migrate db:seed
 
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
