a33c1f4335034d51dac2af9a472d0dae968e5c76
JARVIS-VOVA <jarvis.vova@gmail.com>
2025-07-10 00:56:52 +0300

    Remove archived files (node and docker setup). Already tagged

--- archive/api-node/app/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const currentUser = require('./currentUser')
-const sessions = require('./sessions')
-const users = require('./users')
-
-module.exports = {
-  currentUser,
-  sessions,
-  users,
-}
--- archive/api-node/app/services/index.js
+++
@@ -1,9 +0,0 @@
-const CurrentUserService = require('./currentUser')
-const SessionService = require('./session')
-const UserService = require('./user')
-
-module.exports = {
-  CurrentUserService,
-  SessionService,
-  UserService,
-}
